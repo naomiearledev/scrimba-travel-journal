7bdf6e4901c3e3a36202dae695b6d852d9813129
Naomi Earle <naomiearle@Markbook-Bro.local>
2023-07-02 14:05:45 +0100

    header complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,10 @@
+import icon from "../assets/images/icon.png"
+
 export default function Header() {
   return (
-    <p>Header</p>
+    <div className="header">
+      <img src={icon} alt="icon" />
+      <h1>my travel journal.</h1>
+    </div>
   )
 }
